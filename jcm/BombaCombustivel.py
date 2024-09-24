a48518ce413cf9a39f6dfe3ca76bbee168a0372d
João Vitor <joaovitorrodriguesgomes03@gmail.com>
2024-09-24 18:59:32 -0300

    Git

--- jcm/BombaCombustivel.py
+++ jcm/BombaCombustivel.py
@@ -40,37 +40,3 @@ class BombaCombustivel:
     @quantidade_disponivel.setter
     def quantidade_disponivel(self, quantidade):
         self._quantidade_disponivel = quantidade
-
-
-class BombaEtanol(BombaCombustivel):
-    def __init__(self, valor_litro, quantidade_disponivel):
-        super().__init__(valor_litro, quantidade_disponivel)
-
-
-class BombaGasolina(BombaCombustivel):
-    def __init__(self, valor_litro, quantidade_disponivel):
-        super().__init__(valor_litro, quantidade_disponivel)
-    
-    def abastecer_por_valor_com_aditivo(self, valor):
-        if valor <= 0:
-            return -1
-        
-        valor_litro_aditivo = self._valor_litro * 1.05
-        litros = valor / valor_litro_aditivo
-        if litros > self._quantidade_disponivel:
-            return -1
-        
-        self._quantidade_disponivel -= litros
-        return litros
-    
-    def abastecer_por_litro_com_aditivo(self, litros):
-        if litros <= 0:
-            return -1
-        
-        valor_litro_aditivo = self._valor_litro * 1.05
-        valor = litros * valor_litro_aditivo
-        if litros > self._quantidade_disponivel:
-            return -1
-        
-        self._quantidade_disponivel -= litros
-        return valor
---
+++ jcm/BombaEtanol.py
@@ -0,0 +1,3 @@
+class BombaEtanol(BombaCombustivel):
+    def __init__(self, valor_litro, quantidade_disponivel):
+        super().__init__(valor_litro, quantidade_disponivel)
